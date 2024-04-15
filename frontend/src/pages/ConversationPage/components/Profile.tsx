1a6bccd8543bfb5143faff4c5372f638912b1412
matikant <fe-2003@hotmail.com>
2024-04-15 20:55:37 +0700

    feat: log out button without editing is_connected in db

--- frontend/src/pages/ConversationPage/components/Profile.tsx
+++ frontend/src/pages/ConversationPage/components/Profile.tsx
@@ -1,4 +1,9 @@
+import { environment } from "../../../common/constants/environment"
+import { LocalStorageUtils } from "../../../common/utils/LocalStorageUtils"
 import Button from "./Button"
+import EditProfileIcon from "./EditProfileIcon"
+import { useNavigate } from "react-router-dom"
+import io from "socket.io-client"
 
 export default function Profile({
   userPicture,
@@ -9,16 +14,31 @@ export default function Profile({
   userName: string | null
   onClick: () => void
 }) {
+  const navigate = useNavigate()
+  const socket = io(environment.backend.url)
+
+  const handleLogout = () => {
+    socket.emit("connected-user")
+    LocalStorageUtils.removeData("token")
+    navigate("/home")
+  }
+
   return (
     <div className="flex items-center justify-start w-full gap-4">
       <img
         className="h-20 w-20 object-cover rounded-full"
-        src={userPicture}
+        src={userPicture || ""}
         alt="userPicture"
       ></img>
       <div className="flex flex-col items-start gap-1">
-        <p className="text-gray-900 font-semibold text-xl">{userName}</p>
-        <Button onClick={onClick} buttonText="Edit profile 🖋️" />
+        <div className="flex gap-2">
+          <p className="text-gray-900 font-semibold text-xl">{userName}</p>
+          <button onClick={onClick}>
+            <EditProfileIcon />
+          </button>
+        </div>
+
+        <Button onClick={handleLogout} buttonText="Log out 👋" />
       </div>
     </div>
   )
